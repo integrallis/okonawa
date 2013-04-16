bd049211dadf9dcb3914cad26a4d614022e22d56
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-04-15 23:11:34 -0700

    Passing test: Todo Model - is invalid without a name

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,12 @@
 class Todo
   include MotionModel::Model
   include MotionModel::ArrayModelAdapter
+  include MotionModel::Validatable
 
   columns :name     => :string,
           :details  => :string,
           :due_date => {:type => :date, :formotion => {:picker_type => :date_time}},
           :done     => {:type => :boolean, :default => false, :formotion => {:type => :switch}}
+          
+  validates :name, :presence => true
 end
