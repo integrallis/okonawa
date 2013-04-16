2c76d9e07a89d1cecaef3014e5b786c2fa0ab29d
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-04-15 23:02:42 -0700

    Passing test: Todo Model - has a name, description, a due date and whether is done or not

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,9 @@
 class Todo
+  include MotionModel::Model
+  include MotionModel::ArrayModelAdapter
 
+  columns :name     => :string,
+          :details  => :string,
+          :due_date => {:type => :date, :formotion => {:picker_type => :date_time}},
+          :done     => {:type => :boolean, :default => false, :formotion => {:type => :switch}}
 end
