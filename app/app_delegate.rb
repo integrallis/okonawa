c5950c399414a2a86e383f63c06730b894fa67bf
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-04-15 20:08:27 -0700

    Passing test: Todos Controller - should exist

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,6 +3,9 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
     
+    @table = UITableView.alloc.initWithFrame(UIScreen.mainScreen.bounds) 
+    @window.addSubview(@table)
+    
     true
   end
 end
