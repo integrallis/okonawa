9a58c6a3f28aaeb7da7452368063631f16e11b15
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-04-15 20:24:35 -0700

    Passing test: Todos Controller - displays the given ToDos

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,8 +4,34 @@ class AppDelegate
     @window.makeKeyAndVisible
     
     @table = UITableView.alloc.initWithFrame(UIScreen.mainScreen.bounds) 
+    
+    todos = %w(Milk Orange\ Juice Apples Bananas Brocolli Carrots Beef Chicken Enchiladas Hot\ Dogs Butter Bread Pasta Rice)
+    todos.map! { |thing| "Buy #{thing}"}
+    
+    @data_source = TodosDataSource.new
+    
+    @data_source.data = todos
+    
+    @table.dataSource = @data_source
+    
     @window.addSubview(@table)
     
     true
   end
+end
+
+class TodosDataSource
+  
+  attr_writer :data
+  
+  def tableView(tableView, numberOfRowsInSection:section)
+    @data.size
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath:indexPath)
+    cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:nil)
+    cell.textLabel.text = @data[indexPath.row]
+    cell
+  end
+
 end
