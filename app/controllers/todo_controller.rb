3ea974135054abf2611e044d5ff373f32342b51d
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-04-16 00:19:49 -0700

    Passing test: Todo Controller - saves changes made to a todo

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -8,4 +8,23 @@ class TodoController < Formotion::FormController
     self.todo = todo
   end
 
+  def viewDidLoad
+    super
+    saveButton = UIBarButtonItem.alloc.initWithTitle("Save", style: UIBarButtonItemStyleBordered, target:self, action:'save')
+    self.navigationItem.rightBarButtonItem = saveButton                                                                                     
+  end
+  
+  def save
+    data = @form.render
+    @todo.from_formotion!(data)
+    @todo.save
+    
+    app = UIApplication.sharedApplication
+    delegate = app.delegate
+    controller = delegate.instance_variable_get("@todos_controller")
+    view = controller.instance_variable_get("@table")
+    view.setNeedsDisplay
+    
+    self.navigationController.popToRootViewControllerAnimated(true) unless RUBYMOTION_ENV == 'test'
+  end
 end
