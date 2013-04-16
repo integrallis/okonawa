908bd3d29cca7f45365de362c486e75a29817f55
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-04-15 23:00:55 -0700

    Failing test: Todo Model - has a name, description, a due date and whether is done or not

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -1,5 +1,18 @@
 describe "Todo Model" do
+  
+  before do
+    @todo = Todo.new 
+  end
+
   it "exists" do
     Object.const_defined?('Todo').should.be.true 
   end
+
+  it "has a name, description, a due date and whether is done or not" do
+    @todo.should.respond_to :name
+    @todo.should.respond_to :description
+    @todo.should.respond_to :due_date
+    @todo.should.respond_to :done
+  end
+
 end
