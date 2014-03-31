67b5d433b12f6b5affe5537052bdba94a7ffde97
Brian Sam-Bodden <bsbodden@integrallis.com>
2014-03-31 15:41:12 -0700

    Updated swipe to delete spec for ios 7 and updated all gems

--- spec/todos_controller_spec.rb
+++ spec/todos_controller_spec.rb
@@ -1,6 +1,6 @@
 describe "Todos Controller" do
   tests TodosController
-  
+
   before do
     Todo.delete_all
     @todo = Todo.create :name => 'Buy Milk',
@@ -9,31 +9,31 @@ describe "Todos Controller" do
     @table = controller.tableView
     @todos = controller.instance_variable_get('@todos')
   end
-  
+
   it 'should exist' do
     @table.should.not == nil
   end
-  
+
   it 'displays the given ToDos' do
     @table.visibleCells.should.not.be.empty
   end
-  
+
   it 'displays the correct label for a give ToDo' do
     first_cell = @table.visibleCells.first
     first_cell.textLabel.text.should == 'Buy Milk'
   end
-  
+
   it 'creates a new row for a new todo' do
     controller.add_todo
     last_cell = @table.visibleCells.last
     last_cell.textLabel.text.should == 'New Todo'
   end
-  
-  it 'should be able to delete ' do
-    flick 'Buy Milk', :from => :left, :to => :right, :duration => 1
+
+  it 'should be able to swipe to delete' do
+    flick 'New Todo', :from => :right, :to => :left, :duration => 1.0
     tap 'Delete'
-    todo = Todo.where(:name).eq('Buy Milk').first
+    todo = Todo.where(:name).eq('New Todo').first
     todo.should.be.nil
   end
-  
+
 end
